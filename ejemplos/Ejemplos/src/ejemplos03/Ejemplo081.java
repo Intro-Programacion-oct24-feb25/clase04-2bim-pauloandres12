f1a2302187c07831cd2a5e1822188c165f205c64
Paulo Andres <pbustamante@utpl.edu.ec>
2025-01-08 12:24:56 -0500

    cambios

---
+++ ejemplos/Ejemplos/src/ejemplos03/Ejemplo081.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplos03;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo081 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        // 
+        String mensajeSuma = "";
+        String mensajeSumaDos = "";
+        
+        int opcion; 
+        int limite; 
+        int tabla; 
+        
+        System.out.println("Ingrese la opcion que desea usar");
+        opcion = entrada.nextInt();
+        System.out.println("Ingrese la tabla que desea usar");
+        tabla = entrada.nextInt();
+        System.out.println("Ingrese el limite de la tabla que desea usar");
+        limite = entrada.nextInt();
+        
+        
+        switch(opcion){
+            case 1: mensajeSuma = obtenerTablaSumar (limite, tabla);
+            break;
+            case 2: mensajeSumaDos = obtenerTablaMultiplicar(limite, tabla);
+            break;
+        }
+        
+         // se invoca al método 
+                                                       // obtenerTablaSumar
+                                                       // y el valor que 
+                                                       // devuelve se lo 
+                                                       // almacena  en mensajeSuma
+        
+        System.out.printf("%s\n", mensajeSuma);
+        System.out.printf("%s\n", mensajeSumaDos);
+    }
+        
+    public static String obtenerTablaSumar(int limite, int tabla){
+        int operacion;
+        String cadena = "";
+        for (int i = 1; i <= limite; i++) {
+            operacion = tabla + i;
+            cadena = String.format("%s%d + %d = %d\n", cadena, tabla, i, 
+                    operacion);
+        }
+        return cadena;        
+    }
+    
+    public static String obtenerTablaMultiplicar(int limite, int tabla){
+        int operacion;
+        String cadena = "";
+        for (int i = 1; i <= limite; i++) {
+            operacion = tabla * i;
+            cadena = String.format("%s%d * %d = %d\n", cadena, tabla, i, 
+                    operacion);
+        }
+        return cadena;
+        
+    }
+    
+}
