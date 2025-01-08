220b60b5854837954638f8c394084c45df342bd6
Paulo Andres <pbustamante@utpl.edu.ec>
2025-01-08 11:48:10 -0500

    cambios

--- ejemplos/Ejemplos/src/ejemplos01/Ejemplo01.java
+++ ejemplos/Ejemplos/src/ejemplos01/Ejemplo01.java
@@ -20,11 +20,16 @@ public class Ejemplo01 {
         // String ciudad = "Loja";
         // String miCiudad = obtenerCiudadMayuscula(ciudad);
         // System.out.printf("%s\n", miCiudad);
-        obtenerMultiplicacion(7, 50);
+        //obtenerMultiplicacion(7, 50);
         // 
         
-        String miMensaje = obtenerMultiplicacionDos(7, 50);
-        System.out.printf("%s\n", miMensaje);
+        //String miMensaje = obtenerMultiplicacionDos(7, 50);
+        //System.out.printf("%s\n", miMensaje);
+        
+        String ciudad = "Loja";
+        String ciudad2;
+        ciudad2 = obtenerCiudadMayuscula(ciudad); 
+        System.out.printf("%s\n", ciudad2);
     }
     
     
