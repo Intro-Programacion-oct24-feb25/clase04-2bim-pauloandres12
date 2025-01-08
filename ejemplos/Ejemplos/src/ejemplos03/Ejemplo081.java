d69f2cc74a59c3da46f08f657bcbc8cfc22ea912
Paulo Andres <pbustamante@utpl.edu.ec>
2025-01-08 12:28:39 -0500

    cambios ordenados

--- ejemplos/Ejemplos/src/ejemplos03/Ejemplo081.java
+++ ejemplos/Ejemplos/src/ejemplos03/Ejemplo081.java
@@ -18,57 +18,57 @@ public class Ejemplo081 {
         // 
         String mensajeSuma = "";
         String mensajeSumaDos = "";
-        
-        int opcion; 
-        int limite; 
-        int tabla; 
-        
+
+        int opcion;
+        int limite;
+        int tabla;
+
         System.out.println("Ingrese la opcion que desea usar");
         opcion = entrada.nextInt();
         System.out.println("Ingrese la tabla que desea usar");
         tabla = entrada.nextInt();
         System.out.println("Ingrese el limite de la tabla que desea usar");
         limite = entrada.nextInt();
-        
-        
-        switch(opcion){
-            case 1: mensajeSuma = obtenerTablaSumar (limite, tabla);
-            break;
-            case 2: mensajeSumaDos = obtenerTablaMultiplicar(limite, tabla);
-            break;
+
+        switch (opcion) {
+            case 1:
+                mensajeSuma = obtenerTablaSumar(limite, tabla);
+                break;
+            case 2:
+                mensajeSumaDos = obtenerTablaMultiplicar(limite, tabla);
+                break;
         }
-        
-         // se invoca al método 
-                                                       // obtenerTablaSumar
-                                                       // y el valor que 
-                                                       // devuelve se lo 
-                                                       // almacena  en mensajeSuma
-        
+
+        // se invoca al método 
+        // obtenerTablaSumar
+        // y el valor que 
+        // devuelve se lo 
+        // almacena  en mensajeSuma
         System.out.printf("%s\n", mensajeSuma);
         System.out.printf("%s\n", mensajeSumaDos);
     }
-        
-    public static String obtenerTablaSumar(int limite, int tabla){
+
+    public static String obtenerTablaSumar(int limite, int tabla) {
         int operacion;
         String cadena = "";
         for (int i = 1; i <= limite; i++) {
             operacion = tabla + i;
-            cadena = String.format("%s%d + %d = %d\n", cadena, tabla, i, 
+            cadena = String.format("%s%d + %d = %d\n", cadena, tabla, i,
                     operacion);
         }
-        return cadena;        
+        return cadena;
     }
-    
-    public static String obtenerTablaMultiplicar(int limite, int tabla){
+
+    public static String obtenerTablaMultiplicar(int limite, int tabla) {
         int operacion;
         String cadena = "";
         for (int i = 1; i <= limite; i++) {
             operacion = tabla * i;
-            cadena = String.format("%s%d * %d = %d\n", cadena, tabla, i, 
+            cadena = String.format("%s%d * %d = %d\n", cadena, tabla, i,
                     operacion);
         }
         return cadena;
-        
+
     }
-    
+
 }
