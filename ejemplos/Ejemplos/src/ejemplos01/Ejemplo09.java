bd9ee8b6d857b6a5b8f750cf7307d446dbeb746d
Paulo Andres <pbustamante@utpl.edu.ec>
2025-01-09 10:21:18 -0500

    cambios

--- ejemplos/Ejemplos/src/ejemplos01/Ejemplo09.java
+++ ejemplos/Ejemplos/src/ejemplos01/Ejemplo09.java
@@ -15,11 +15,14 @@ public class Ejemplo09 {
         // 
         String mensajeSuma;
         String mensajeSumaDos;
+        String mensajeSumaTres; 
         mensajeSuma = obtenerSuma(10, 9);
         mensajeSumaDos = obtenerSuma(10);
+        mensajeSumaTres = obtenerSuma(11,9,8);
         
         System.out.printf("%s\n", mensajeSuma);
         System.out.printf("%s\n", mensajeSumaDos);
+        System.out.printf("%s\n", mensajeSumaTres);
     }
         
     public static String obtenerSuma(int operandor1, int operandor2){
@@ -37,6 +40,14 @@ public class Ejemplo09 {
         cadena = String.format("%s%d\n", cadena, suma);
         return cadena;        
         
+    }
+    public static String obtenerSuma(int operandor1, int operandor2, 
+            int operandor3){
+        int suma;
+        String cadena = "";
+        suma = operandor1 + operandor2 + operandor3;
+        cadena = String.format("%s%d\n",cadena,suma);
+        return cadena;
     }
     
 }
